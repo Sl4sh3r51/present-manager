bb4249aca27cf1b218361499080a16268d31cbb7
Sl4sh3r51 <dani.swan51@gmail.com>
2026-02-02 17:03:00 +0100

    initialize backend project

---
+++ src/main/java/org/iu/presentmanager/PresentManagerApplication.java
@@ -0,0 +1,13 @@
+package org.iu.presentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresentManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/iu/presentmanager/PresentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iu.presentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresentManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
